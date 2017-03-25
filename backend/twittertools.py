52aab29404f73efcf337a579a1045c0280a72962
bhoberman <ben.hoberman@gmail.com>
2017-03-25 13:13:36 -0700

    Implement Basic Tweet Collection

---
+++ backend/twittertools.py
@@ -0,0 +1,12 @@
+#The scripts for interacting with twitter
+#Uses python-twitter, found at https://github.com/bear/python-twitter
+#Documentation at https://python-twitter.readthedocs.io/en/latest/
+import twitter
+
+api = twitter.Api(consumer_key='ikJIFDBN0h1LZomIkCRKbQXUE', consumer_secret='NS411RIiYBO2Gzj67pqcgFnD0AY3mPV5hWjkujCklZjeLurwDa', access_token_key='280279077-y9OmVTnveqglTJTy2ND2uPCy9wLWj9PnHDA0Kl0j', access_token_secret='HYW79wDKilDY44QX5mGupWx0001V06jz8mWCLt9aYihXe')
+
+#Given a user's @name (screenname), return a list of all of their statuses.
+def get_posts(screenname):
+    global api
+    posts = api.GetUserTimeline(screen_name=screenname, count=200, exclude_replies=True)
+    return posts
